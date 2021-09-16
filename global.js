467adf254ab4f50f457384e28c3f2bbaca97aa99
TheTrueProblematic <mwmttl@gmail.com>
2021-09-16 11:21:41 -0600

    Mobile quick fix

--- global.js
+++ global.js
@@ -7,3 +7,24 @@ function getWidth(id){
     var positionInfo = element.getBoundingClientRect();
     return positionInfo.width;
 }
+function getHeight(id){
+    var element = document.getElementById(id);
+    var positionInfo = element.getBoundingClientRect();
+    return positionInfo.height;
+}
+
+function mobileCheck() {
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+    if (width < height) {
+        location.replace("mobile.html");
+    }
+}
+
+function mobileCheck2() {
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+    if (width > height) {
+        location.replace("index.html");
+    }
+}
